6c3fd2596fd068ecacca7c73fe375913c6480cf8
Vishal <vishalkumartcd@gmail.com>
2021-01-27 11:44:23 +0000

    Initializing workspace using Spring Initializer
    Added dependencies - Spring Web and MySQL Driver

---
+++ src/main/java/com/mishra/shorturlapi/ShortUrlApiApplication.java
@@ -0,0 +1,13 @@
+package com.mishra.shorturlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortUrlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortUrlApiApplication.class, args);
+	}
+
+}
